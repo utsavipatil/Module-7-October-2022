0c74fae790c62e19e8e17952a0569593e2db137f
Utsavi Patil <17bee076@nirmauni.ac.in>
2022-10-12 22:12:34 +0530

    12 October Rendering data from API

--- src/class-7-promices-useEffect/Promise.js
+++ src/class-7-promices-useEffect/Promise.js
@@ -1,26 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Promise() {
 
-    const [userMessages, setUserMessages] = React.useState('')
+    const [userMessages, setUserMessages] = useState('')
     const userMessage = [];
 
-    fetch('https://learning-at-acciojob-default-rtdb.asia-southeast1.firebasedatabase.app/message.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        for (const key in data){
-            userMessage.push(data[key])
-        }
-        setUserMessages(userMessage)
-    })
+    useEffect(()=> {
+        fetch('https://learning-at-acciojob-default-rtdb.asia-southeast1.firebasedatabase.app/message.json')
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data)
+            for (const key in data){
+                userMessage.push(data[key])
+            }
+            console.log(userMessage);
+            setUserMessages(userMessage) //It will re render component and cycle goes on from initial 
+        })
+    }, [])
+
+    console.table(userMessages);
+
+    // fetch('https://learning-at-acciojob-default-rtdb.asia-southeast1.firebasedatabase.app/message.json') //We are not using useEffect so, it will give infinite data
+    // .then(response => response.json())
+    // .then(data => {
+    //     // console.log(data)
+    //     for (const key in data){
+    //         userMessage.push(data[key])
+    //     }
+    //     console.log(userMessage);
+    //     setUserMessages(userMessage) //It will re render component and cycle goes on from initial 
+    // })
 
   return (
     <div>Promise
         {
-            userMessages.map(message => {
+            userMessages && userMessages.map(message => {
                 return (
-                    <div>
+                    <div style={
+                        {
+                            color : "white",
+                            backgroundColor:'orchid',
+                            padding: 20,
+                            margin: 10,
+                            borderRadius:10,
+                            textAlign:'center'
+                        }
+                    }>
                         {message.userName} : {message.userMessage}
                     </div>
                 ) 
